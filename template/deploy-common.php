8ed59113b1076d8c8374aa8bad22671f9bb1625e
Hui Yang <chy1988@antwebstudio.com>
2020-02-04 01:58:00 +0800

    Updated project template files

--- template/deploy-common.php
+++ template/deploy-common.php
@@ -14,6 +14,11 @@ after('deploy:failed', 'deploy:unlock');
 set('bin/yii', '{{bin/php}} yii');
 set('default_timeout', 0);
 
+set('bin/mysqldump', 'mysqldump');
+set('bin/mysql', 'mysql');
+set('bin/gunzip', 'gunzip');
+set('bin/gzip', 'gzip');
+
 task('ssh-add', function() {
 	runLocally('eval $(ssh-agent -s)');
 	runLocally('ssh-add ~/.ssh/id_rsa');
@@ -32,3 +37,46 @@ task('deploy:install', function () {
 	//run("cd {{project_path}}/web && {{bin/symlink}} {{project_path}}/storage/web storage");
 });
 
+task('db:download', function() {
+	if (askConfirmation('Are you sure to overwrite local database using live database ?')) {
+		set('liveFile', '{{application}}_'.date('Y-m-d-His').'.sql.gz');
+		set('liveFilePath', '{{project_path}}/frontend/web/{{liveFile}}');
+		
+		// Backup live db
+		run('{{bin/mysqldump}} -u {{liveDbUser}} -p"{{dbPassword}}" {{db}} | {{bin/gzip}} > {{liveFilePath}}');
+		
+		runLocally('wget {{baseUrl}}/{{liveFile}}');
+		
+		runLocally('{{bin/gunzip}} < {{liveFile}} | mysql -h 127.0.0.1 -D {{localhostDb}} -u root -p"root" --default-character-set=utf8');
+		
+		run('unlink {{liveFilePath}}');
+	}
+});
+
+task('db:update_beta', function() {
+	$hostname = Task\Context::get()->getHost()->getHostname();
+	
+	// ============
+	// WARNING: 
+	// ============
+	// $hostname == 'beta' condition is very important, removing it may cause disaster.
+	
+	if ($hostname == 'beta' && askConfirmation('Are you sure to clone database from '.get('liveDb').' to '.get('db').' ?')) {
+		set('betaFile', 'beta_'.date('Y-m-d-His').'.sql.gz');
+		set('liveFile', 'v2_'.date('Y-m-d-His').'.sql.gz');
+		
+		//run('LIVE_FILE=v2_$(date +%Y%m%d_%H%M%S).sql.gz');
+		//run('BETA_FILE=beta_$(date +%Y%m%d_%H%M%S).sql.gz');
+		
+		// Backup beta db
+		run('{{bin/mysqldump}} -u {{dbUser}} -p"{{dbPassword}}" {{db}} | {{bin/gzip}} > {{betaFile}}');
+		
+		// Backup live db
+		run('{{bin/mysqldump}} -u {{liveDbUser}} -p"{{liveDbPassword}}" {{liveDb}} | {{bin/gzip}} > {{liveFile}}');
+		
+		run('{{bin/mysql}} -u {{dbUser}} -p"{{dbPassword}}" {{db}} -e "DROP DATABASE {{db}}; CREATE DATABASE {{db}}"');
+		
+		// Restore live db to beta db
+		run('{{bin/gunzip}} < {{liveFile}} | mysql -D {{db}} -u {{dbUser}} -p"{{dbPassword}}" --default-character-set=utf8');
+	}
+});
---
+++ template/deploy.php
@@ -0,0 +1,37 @@
+<?php
+namespace Deployer;
+
+require 'deploy-common.php';
+
+// Project repository
+//set('repository', 'git@bitbucket.org:antwebstudio/ruangkongsi.git');
+
+// Project name
+set('project_path', '{{release_path}}');
+
+// Shared files/dirs between deploys 
+set('shared_files', []);
+set('shared_dirs', [
+	'storage',
+]);
+
+// Writable dirs by web server 
+set('writable_dirs', []);
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+	'deploy:install',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
